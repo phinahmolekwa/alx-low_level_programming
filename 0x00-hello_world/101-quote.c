f2f4e10d857b3af33acb73883068b19ef9cefd13
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-10 19:35:11 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
 *main-Entry point
-*Return: 1
+*Return: 0
 */
 int main(void)
 {
-char ch[] = "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n";
-write (2, ch, sizeof(ch));
+write(1, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
