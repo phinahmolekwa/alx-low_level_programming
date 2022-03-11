5443622742a6fa988c72e015fb291be2ec1cb046
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-10 19:19:15 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-main block
+*Return: 1
+*/
+int main(void)
+{
+char ch[]="and that piece of artis useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
+}
