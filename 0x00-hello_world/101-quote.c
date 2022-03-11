c7384c08bf3c9af8e2f82fb4e7a6ff421d2e07f2
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-10 19:21:59 -0800

    quote unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main-main block
-*Return: 1
+*Return: 0
 */
 int main(void)
 {
 char ch[]="and that piece of artis useful\" - Dora Korpar, 2015-10-19\n";
 write(2, ch, sizeof(ch));
-return (1);
+return (0);
 }
