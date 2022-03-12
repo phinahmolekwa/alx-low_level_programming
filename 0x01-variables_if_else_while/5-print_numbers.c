2afe9035aebe38a82fd495ee3024cb64664a016c
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 17:40:47 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *Return: 0
+ *prints single digit numbers
+ */
+int main(void)
+{
+  char c = '0';
+  while ( char c <= '9')
+    putchar(char c);
+      c++;
+
+      putchar('\n');
+      return (0)
+	}
