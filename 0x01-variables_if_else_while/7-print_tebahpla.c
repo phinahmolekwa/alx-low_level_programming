4ca707da7da771b30f83fe71120d3501445c819f
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 17:54:11 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main-entry point
+ *Return: 0
+ */
+int main(void)
+{
+  char i = 'z';
+  while (i >= 'a')
+    putchar(i);
+  i--;
+
+  putchar('\n');
+  return (0);
+}
