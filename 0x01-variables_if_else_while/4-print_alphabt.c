856e2c3528f6018460b66c74fd4fa552fa9b85bf
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 18:44:53 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-  char c = 'a';
+char c = 'a';
 
-  while (c<= 'z')
-    
-    if (c != 'q' && c != 'e')
+while (c <= 'z')
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+c++;
+}
 
-      putchar(c);
-  c++;
-
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
