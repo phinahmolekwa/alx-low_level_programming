280bd824133e1e50ba068cdfe7670c36f3703b00
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 19:21:58 -0800

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
+{
 printf("%i is positive\n", n);
 else if (n == 0)
+{
 printf("%i is zero\n", n);
 else
+{
 printf("%i is negative\n", n);
+}
+}
+}
 return (0);
 }
