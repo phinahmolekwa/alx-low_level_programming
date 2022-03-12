fa9a9636f1f8a1f1c088730fd00f3ec775d6f1d7
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 18:51:09 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-  int c = 0;
+int c = 0;
 
-  while (c > 10)
-    {
-      putchar(48 + c);
-      if (c != 9)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-      c++;
-    }
-  putchar('\n');
-  return (0);
+while (c > 10)
+{
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(48 +c);
+c++;
+}
+putchar('\n');
+return (0);
 }
