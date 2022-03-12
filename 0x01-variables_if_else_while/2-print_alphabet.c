1b9ca4952319fe0fb59f5b122893d8437b59c222
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 17:11:13 -0800

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  *main - entry point
  * Return: 0
  */
-
 int main(void)
 {
- 
-char c;
-c = 'a';
+  
+char c = 'a';
   
 while (c <= 'z')
+{
 putchar(c);
 c++;
-
+}
 putchar('\n');
 return (0);
 }
