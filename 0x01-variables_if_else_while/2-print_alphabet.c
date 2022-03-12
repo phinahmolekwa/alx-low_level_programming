5ac4565c4a797330a47efb253f323bec571b158e
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 16:36:09 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+c = 'a';
+  
+while (c <= 'z')
+putchar(c);
+c++;
+putchar('\n');
+return (0);
+}
+
+  
