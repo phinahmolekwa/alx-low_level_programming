906d126374d70a187857d876350bdb5e8505f221
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-11 18:08:14 -0800

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-  char c;
-  char d = '0';
+char c;
+char d = '0';
 
-  while (d <= '9')
-    {
-      putchar(d);
-      d++;
-    }
-  for (c ='a'; c <= 'f'; c++)
-    {
-      putchar(c);
-    }
-  putchar('\n');
-  return (0);
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+for (c ='a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
