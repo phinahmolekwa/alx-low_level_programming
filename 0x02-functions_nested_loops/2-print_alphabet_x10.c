7ff28c143530546352f361f9de61a6a970c86de4
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-15 14:34:09 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ *main-entry point
+ *Return: 0
+ *prints 10x the alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+  int b = 0;
+  while (b < 10)
+    {
+      char a = 'a';
+
+      while (a <= 'z')
+	{
+	  _putchar(a);
+	  a++;
+	}
+      _putchar('\n');
+      b++;
+    }
+}
