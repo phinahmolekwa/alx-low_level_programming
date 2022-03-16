ef5dd56d1a62afefa9e3c8a18fab711146fff291
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-16 06:15:55 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,13 @@
  */
 
 void print_alphabet(void)
-  
-{
-  char alphabet;
 
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      _putchar(alphabet);
-    }
-  
-  _putchar('\n');
- 
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
 }
