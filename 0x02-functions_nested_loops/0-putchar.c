a3d1d1bc78e68bf7e9c0e1dea583b2f5a481102e
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-16 05:27:45 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,20 @@
 
 /**
  *main - print _putchar
- *Return: 0
+ *
+ *Return: 0 (Succes)
  */
 
 int main(void)
 {
-  char text[9] = "_putchar";
-
-  int i = 0;
-  for (i = 0;  i < 8; i++)
-    {
-      _putchar(text[i]);
-    }
-  _putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');  
+return (0);
 }
