d197d4051106f864def5796a44fbaed2ff93f144
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-16 08:15:38 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 
 /**
  *print_last_digit - prints the last digit of a number
+ *@n: the integer to print
  *
  *Return: integer
  */
 
 int print_last_digit(int n)
 {
-  int last = n % 10;
+int last = n % 10;
 
-  if (n < 0)
-    {
-      last = last * -1;
-
-      _putchar(last + '0');
-    }
-  return (last);
+if (n < 0)
+{
+last = last * -1;
+_putchar(last + '0');
+}
+return (last);
 }
