34d0de2ff6d27fad402263e699d69c3b434ea209
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-17 15:13:28 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_line - function that draws a straight line in the terminal
+ *@n: is the number of times the character _ should be printed
+ *Return: 0 Always
+ */
+
+void print_line(int n)
+{
+int i = 0;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
