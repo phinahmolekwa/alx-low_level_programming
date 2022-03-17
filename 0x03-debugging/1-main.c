30ec34db03d2afd4f0ff994b1756aa892e0feebd
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-17 13:24:00 -0700

    infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,17 +10,7 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
-/*
-*while (i < 10)
-*{
-*_putchar(i);
-*}
-*/
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
