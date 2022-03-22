5509c22d6ec9e949fb58f0f4316e41b1eaf0de73
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-22 15:49:29 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen - lenght of a string
+ *@s: A pointer to an int that will be updated
+ *
+ *Return: 0 Always
+ */
+
+int _strlen(char *s)
+{
+int c;
+
+u = 0;
+
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
+}
