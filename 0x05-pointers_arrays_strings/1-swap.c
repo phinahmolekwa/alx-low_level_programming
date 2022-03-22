7a588d52603530f28f9d629572477b84fc17405b
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-22 15:47:57 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - swap the values of two integers
+ *@a: A pointer to an int that will be updated
+ *@b: B pointer to an int that will be updated
+ *Return: 0 Always
+ */
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+
+*a + *b;
+
+*b = c;
+}
