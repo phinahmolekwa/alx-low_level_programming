6288dcacdbf0a0036c837235ceb69bc958e075eb
phinahmolekwa <mapula.phinah@gmail.com>
2022-03-24 15:35:50 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,16 +13,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-i = 0;
-while (i < n && src[i] != '\0')
-{
+
+for (i = 0; src[i] != '\0' && i < n ; i++)
 dest[i] = src[i];
-i++;
-}
+
 while (i < n )
 {
-dest[i] = '\0';
-i++;
+dest[i++] = '\0';
 }
 return (dest);
 }
