41fe2625dc4498d60ec090890cb4ffd757369217
phinahmolekwa <mapula.phinah@gmail.com>
2022-04-07 16:57:30 -0700

    my commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,22 +12,32 @@ int *p;
 int size, i, j;
 
 if (min > max)
+{
 return (0);
+}
 
 if (max > min)
+{
 size = max - min +1;
+}
 else if (max == min)
+{
 size = 2;
-
+}
 p = malloc(sizeof(int) * size);
 
 if (p == 0)
+{
 return (0);
-
+}
 for (i = 0; j = min; j <= max; i++, j++)
+{
 p[i] = j;
+}
 
 if (max == min)
+{
 p[i] = max;
+}
 return (p);
 }
