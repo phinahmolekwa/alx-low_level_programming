906b8c5976a1ac1a9ce9d8054141391917e661ff
phinahmolekwa <mapula.phinah@gmail.com>
2022-04-07 18:36:16 -0700

    my commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,67 @@
 #include "main.h"
 
 /**
+ *_puts - prints a string, followed by a new line
+ *@str: pointer to the string to print
+ *Return: void
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+while (str[i])
+{
+_putchar(str[i]);
+i++;
+}
+}
+
+/**
+ *_atoi - convert a string to an integer
+ *@s: char type string
+ *Return: converted integer
+ */
+
+int _atoi(const char *s)
+{
+int sign = 1;
+unsigned long int resp = 0, firstNum, i;
+for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+{
+if (s[firstNum] == '-')
+{
+sign *= -1;
+}
+}
+for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+{
+resp *= 10;
+resp += (s[i] - 48);
+}
+return (sign * resp);
+}
+
+
+/**
+ *print_int - prints integer
+ *@n: int
+ *Return: 0
+ */
+
+void print_int(unsigned long int n)
+{
+unsigned long int divisor = 1, i, resp;
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+resp = n / divisor;
+_putchar('0' + resp);
+}
+}
+
+
+/**
  *main - print the results of multiplication
  *@argc: n arguments
  *@argv: args
@@ -9,25 +70,15 @@
 
 int main(int argc, char *argv[])
 {
-unsigned long mul;
-int i, j;
+  (void)argc;
 if (argc != 3)
 {
-printf("Error\n");
+_puts("Error");
 exit(98);
 }
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{
-printf("Error\n");
-exit(98);
-}
-}
-}
-mul = atoi(argv[1]) * atoi(argv[2]);
-printf("%lu\n", mul);
+
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
+
 return (0);
 }
